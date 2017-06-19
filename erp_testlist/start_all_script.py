c2dcaff0990ddea50a102ccb5b4c560ae7768a13
shenwei <my126sw@126.com>
2017-06-19 11:20:52 +0800

    20170619_first

--- erp_testlist/start_all_script.py
+++ erp_testlist/start_all_script.py
@@ -58,24 +58,13 @@ def creatsuite():
 now_time = time.strftime("%Y%m%d%H%M%S", time.localtime(time.time()))
 file_name = (os.getcwd() + '/all_script/data/html_result/')+ now_time + 'result.html'
 fp = file(file_name, 'wb')
-
-#========================将测试用例添加多进程,开始运行并生成测试报告========================
-def EEEEEmultiRunCase(dir):
-	suite = dir[0]
-	proclist=[]
-	s=0
-	for i in suite:
-		runner =HTMLTestRunner.HTMLTestRunner(stream=fp,title=u'测试报告',description=u'用例执行情况：' )
-		proc =threading.Thread(target=runner.run(i),args=(i,))
-		proclist.append(proc)
-		s=s+1
-	for proc in proclist:
-		proc.start()
-	for proc in proclist:
-		proc.join()
+runner =HTMLTestRunner.HTMLTestRunner(
+	stream=fp,
+	title=u'测试报告',
+	description=u'用例执行情况：' )
 
 if __name__ == '__main__':
 	alltestnames = creatsuite()
-	EEEEEmultiRunCase(alltestnames)
+	runner.run(alltestnames[0])
 	# sendreport()
 	fp.close()
